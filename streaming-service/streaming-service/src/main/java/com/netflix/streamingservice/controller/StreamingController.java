05068d594532a20bbf781607ad9bf7989bb4a60f
Abhishek <abhisheksharmacky@gmail.com>
2026-07-05 08:44:39 +0530

    Streaming Service

--- streaming-service/streaming-service/src/main/java/com/netflix/streamingservice/controller/StreamingController.java
+++ streaming-service/streaming-service/src/main/java/com/netflix/streamingservice/controller/StreamingController.java
@@ -1,4 +1,52 @@
 package com.netflix.streamingservice.controller;
 
+import com.netflix.streamingservice.dto.StreamingResponse;
+import com.netflix.streamingservice.service.StreamingService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/stream")
+@Slf4j
+@RequiredArgsConstructor
+
 public class StreamingController {
+
+    private final StreamingService streamingService;
+    private final RedisTemplate<String,String> redisTemplate;
+
+    private static final String MASTER_PLAYLIST_KEY_PREFIX = "streaming:playlist:";
+    /**
+     * Get Streaming URL for a movie
+     *  Returns Presigned HLS Master playlist URL
+     *
+     *  GET /api/v1/stream/{movieId}
+     */
+    @GetMapping("/{movieId}")
+    public ResponseEntity<StreamingResponse>getStreamingUrl(
+            @PathVariable String movieId){
+        log.info("Streaming request for movie: {}",movieId);
+
+        //get master playlist key from redis
+        String playlistKey = redisTemplate.opsForValue()
+                .get(MASTER_PLAYLIST_KEY_PREFIX + movieId);
+
+        if(playlistKey == null){
+        return ResponseEntity.notFound().build();
+        }
+
+        StreamingResponse response = streamingService.getStreamingUrl(movieId,playlistKey);
+        return ResponseEntity.ok(response);
+
+
+    }
+
+
 }
--- streaming-service/streaming-service/src/main/java/com/netflix/streamingservice/service/StreamingService.java
+++ streaming-service/streaming-service/src/main/java/com/netflix/streamingservice/service/StreamingService.java
@@ -1,5 +1,49 @@
 package com.netflix.streamingservice.service;
 
+import com.netflix.streamingservice.dto.StreamingResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class StreamingService {
+
+    private final S3Presigner s3Presigner;
+    private final RedisTemplate<String ,String> redisTemplate;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    @Value("${aws.s3.presigned-url-expiry}")
+    private long presignedUrlExpiry;
+
+    //Redis key for caching streaming URLs
+    private final static String Streaming_URL_CACHE_PREFIX = "streaming:url:";
+
+    /**
+     * Get Streaming Url for a movie
+     *
+     *  * FLOW:
+     *  * 1. Check Redis cache for existing presigned URL.
+     *  * 2. If cached - return immediately.
+     *  * 3. If not cached - generate new presigned URL from S3.
+     *  * 4. Cache the URL in Redis.
+     *  * 5. Return streaming URL.
+     *  *
+     *  * Why presigned URL?
+     *  * - S3 bucket is private (locker room) - videos are not publicly accessible.
+     *  * - Presigned URL gives temporary access (X minutes).
+     *  * - Prevent unauthorized video downloads.
+     *  */
+
+    public StreamingResponse getStreamingUrl(String movieId,String playlistKey) {
+
+    }
+
 
 }
